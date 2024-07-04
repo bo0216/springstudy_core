f1f58ab82ac188ed91a11aa3fd6aa228064a24f4
bo0216 <bo216@naver.com>
2024-07-05 02:11:50 +0900

    spring core 학습 최초 생성

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    /*
+    * MemberServiceImpl은 MemberRepository의 추상화에도 의존,
+    * MemoryMemberRepository 구체화에도 의존
+    * => DIP 위반!!
+    * */
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 단일 원칙 잘 지킨 예. 할인 정책이 바뀌면 discountPolicy내용만 바꿔주면 된다
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
